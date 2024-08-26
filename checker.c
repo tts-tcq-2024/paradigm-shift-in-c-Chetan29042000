3028b771eb4b14291dc77f9c1926c818e575097c
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-08-26 10:57:48 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -26,7 +26,7 @@ int check_chargeRate(float chargeRate){
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return check_temperature && check_soc && check_chargeRate;
+  return check_temperature(temperature) && check_soc(soc) && check_chargeRate(chargeRate);
 }
 
 int main() {
